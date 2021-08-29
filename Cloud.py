fbb14917c6fe966125c8db01e53271b5d52e9938
Vlad <vlad.romanescu@hotmail.com>
2021-08-29 21:08:07 +0300

    Edited with VIM and BASH

---
+++ Cloud.py
@@ -0,0 +1,20 @@
+import random
+
+number = []
+
+while len(number) < 6:
+number.append(random.randint(1,100))
+
+set_number = set(number)
+
+while len(set_number) != len(number):
+    x = random.randint(1,100)
+
+    if x in set_number:
+        pass
+    else:
+        set_number = list(number)
+        set_number.append(x)
+    
+    
+print (set_number, /n "Edited Using VIM and BASH")
